440c27811d98c9f246e2292c7f775d3024ce385d
Heiko Braun <ike.braun@googlemail.com>
2016-05-12 17:32:37 +0200

    cleanup

--- microservice/order/src/main/java/org/javaee7/wildfly/samples/everest/order/Main.java
+++ microservice/order/src/main/java/org/javaee7/wildfly/samples/everest/order/Main.java
@@ -3,9 +3,7 @@ package org.javaee7.wildfly.samples.everest.order;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
 import org.wildfly.swarm.Swarm;
-import org.wildfly.swarm.datasources.DatasourcesFraction;
 import org.wildfly.swarm.jaxrs.JAXRSArchive;
-import org.wildfly.swarm.jpa.JPAFraction;
 
 /**
  * @author Heiko Braun
@@ -16,34 +14,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         Swarm swarm = new Swarm();
-
-        swarm.fraction(
-                new JPAFraction()
-                        .inhibitDefaultDatasource()
-                        .defaultDatasource("jboss/datasources/ExampleDS")
-        );
-
-        swarm.fraction(
-                new DatasourcesFraction()
-                        .jdbcDriver("h2", (d) -> {
-                            d.driverClassName("org.h2.Driver");
-                            d.xaDatasourceClass("org.h2.jdbcx.JdbcDataSource");
-                            d.driverModuleName("com.h2database.h2");
-                        })
-                        .dataSource("ExampleDS", (ds) -> {
-                            ds.driverName("h2");
-                            ds.connectionUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
-                            ds.userName("sa");
-                            ds.password("sa");
-                        })
-        );
-
         swarm.start();
 
         JAXRSArchive archive = ShrinkWrap.create(JAXRSArchive.class);
         archive.addPackage(Main.class.getPackage());
-        archive.addAllDependencies();
-
         archive.addAsWebInfResource(new ClassLoaderAsset("META-INF/persistence.xml", Main.class.getClassLoader()), "classes/META-INF/persistence.xml");
 
         swarm.deploy(archive);
