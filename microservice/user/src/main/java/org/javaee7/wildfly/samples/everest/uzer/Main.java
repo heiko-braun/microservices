73b1cc02def6156e81286fb8356a4dcbb85da5e3
Heiko Braun <ike.braun@googlemail.com>
2016-05-17 12:08:27 +0200

    Added project-stages.yml to user service

--- microservice/user/src/main/java/org/javaee7/wildfly/samples/everest/uzer/Main.java
+++ microservice/user/src/main/java/org/javaee7/wildfly/samples/everest/uzer/Main.java
@@ -1,5 +1,7 @@
 package org.javaee7.wildfly.samples.everest.uzer;
 
+import java.net.URL;
+
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
 import org.wildfly.swarm.Swarm;
@@ -13,7 +15,10 @@ import org.wildfly.swarm.topology.TopologyArchive;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        Swarm swarm = new Swarm();
+        URL stageConfig = Main.class.getClassLoader().getResource("project-stages.yml");
+
+        Swarm swarm = new Swarm()
+                .withStageConfig(stageConfig);
         swarm.start();
 
         JAXRSArchive archive = ShrinkWrap.create(JAXRSArchive.class);
@@ -22,7 +27,11 @@ public class Main {
         archive.addAllDependencies();
 
         // advertise service
-        archive.as(TopologyArchive.class).advertise("user");
+        archive.as(TopologyArchive.class).advertise(
+                swarm.stageConfig()
+                        .resolve("service.user.service-name")
+                        .getValue()
+        );
 
         swarm.deploy(archive);
 
