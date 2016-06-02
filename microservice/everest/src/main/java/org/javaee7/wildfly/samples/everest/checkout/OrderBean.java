da49cae02f48c0be373e76d442c50c1ce253397e
Heiko Braun <ike.braun@googlemail.com>
2016-06-02 20:53:38 +0200

    First working ribbon example invocation (nofallback and other gimmicks)

--- microservice/everest/src/main/java/org/javaee7/wildfly/samples/everest/checkout/OrderBean.java
+++ microservice/everest/src/main/java/org/javaee7/wildfly/samples/everest/checkout/OrderBean.java
@@ -1,12 +1,15 @@
 package org.javaee7.wildfly.samples.everest.checkout;
 
 import java.io.Serializable;
+import java.io.StringReader;
+import java.nio.charset.Charset;
 import java.util.List;
-import java.util.concurrent.Future;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.json.Json;
+import javax.json.JsonObject;
 
 import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
 import com.netflix.ribbon.ClientOptions;
@@ -19,7 +22,7 @@ import org.javaee7.wildfly.samples.everest.cart.Cart;
 import org.javaee7.wildfly.samples.everest.cart.CartItem;
 import org.javaee7.wildfly.samples.services.discovery.ServiceDiscovery;
 import rx.Observable;
-import rx.Observer;
+import rx.observables.BlockingObservable;
 
 /**
  * @author arungupta
@@ -51,53 +54,38 @@ public class OrderBean implements Serializable {
         });
 
         try {
-
             // the request context thread locals
             HystrixRequestContext context = HystrixRequestContext.initializeContext();
 
             try {
                 HttpResourceGroup httpResourceGroup = Ribbon.createHttpResourceGroup(
                         "order", // the name of the service in the registry
-                        ClientOptions.create()
-                                .withMaxAutoRetriesNextServer(3)
+                        ClientOptions.create().withMaxAutoRetriesNextServer(3)
                 );
 
-                HttpRequestTemplate<ByteBuf> submitOrderTemplate = httpResourceGroup.newTemplateBuilder("submitOrder", ByteBuf.class)
+                HttpRequestTemplate<ByteBuf> template = httpResourceGroup.newTemplateBuilder("submitOrder", ByteBuf.class)
                         .withMethod("POST")
                         .withUriTemplate("/order/resources/order")
                         /*.withFallbackProvider(new RecommendationServiceFallbackHandler())*/
                         .build();
 
-                Observable<ByteBuf> obs = submitOrderTemplate.requestBuilder()
+                BlockingObservable<ByteBuf> obs = template.requestBuilder()
+                        .withHeader("Content-Type", "application/json")
                         .withContent(Observable.just(Unpooled.wrappedBuffer(order.asJsonString().getBytes())))
                         .build()
-                        .toObservable();
+                        .observe().toBlocking();
 
-                obs.subscribe(new Observer<ByteBuf>() {
-                    @Override
-                    public void onCompleted() {
-                        status = "Order processed successfully";
-                    }
+                String response = new String(obs.last().toString(Charset.forName("UTF-8")));
+                JsonObject jsonResponse = Json.createReader(new StringReader(response)).readObject();
 
-                    @Override
-                    public void onError(Throwable throwable) {
-                        throwable.printStackTrace();
-                        status = "Failed to process order";
-                    }
-
-                    @Override
-                    public void onNext(ByteBuf byteBuf) {
-                        // ignore
-                    }
-                });
+                status = "Order successful, order number: " + jsonResponse.get("orderId");
 
             } finally {
                 context.shutdown();
             }
 
-            cart.clearCart();      // bummer, if the req fails everything is lost
-
         } catch (Exception e) {
+            e.printStackTrace();
             status = e.getLocalizedMessage();
         }
     }
